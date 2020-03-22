c09e2ba93e62847eb165c239ba3f01bab8fed209
Igor Veyner <igorveyner95@gmail.com>
2020-03-22 14:32:31 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -14,7 +14,7 @@ const codes = [
 function init() {
   let index = 0;
   
-  document.body.addEventListener("keydown", (event(e)) => {
+  document.body.addEventListener("keydown", (event) => {
     
   })
 }
